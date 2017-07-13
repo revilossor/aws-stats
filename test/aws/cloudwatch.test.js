9bcf9391cd21ac18f99561dfbe378fe73819aa63
Oliver <oliver.ross@dwp.gsi.gov.uk>
2017-07-13 13:12:06 +0100

    test index uses the lists router

---
+++ test/aws/cloudwatch.test.js
@@ -0,0 +1,5 @@
+describe('test', () => {
+  test('poop', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ test/index.test.js
@@ -0,0 +1,22 @@
+describe('index', () => {
+  const express = require('express');
+  let use;
+  const routerAssertions = [
+    { endpoint: '/list', mock: jest.fn(), path: '../src/router/list' }
+  ];
+
+  beforeAll(() => {
+    routerAssertions.forEach(assertion => jest.mock(assertion.path, () => assertion.mock));
+    use = jest.spyOn(express.Router, 'use');
+    require('../index');
+  });
+
+  describe('routers', () => {
+    routerAssertions.forEach((assertion) => {
+      test(`${assertion.path} used for ${assertion.endpoint}`, () => {
+        expect(use).toHaveBeenCalledWith(assertion.endpoint, assertion.mock);
+      });
+    });
+  });
+
+});
