a89593d2c05dd12027253d7be2306d93b69cd280
Oliver <oliver.ross@dwp.gsi.gov.uk>
2017-08-19 10:24:53 +0100

    fix merge in licence ( derp )

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ const router = require('express').Router();
 router.use('/list', require('./src/router/list'));
 router.use('/stat', require('./src/router/stat'));
 
-// TODO documentation on / ?
-
 module.exports = router;
